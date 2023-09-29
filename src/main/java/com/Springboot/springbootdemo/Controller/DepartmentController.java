04f3151a7c979e22eb5151d5850d115f488fa951
PratikshaKute16 <pratikute1603@gmail.com>
2023-09-29 20:53:03 +0530

    initial changes

--- src/main/java/com/Springboot/springbootdemo/Controller/DepartmentController.java
+++ src/main/java/com/Springboot/springbootdemo/Controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.Springboot.springbootdemo.Controller;
 import com.Springboot.springbootdemo.entitity.Department;
 import com.Springboot.springbootdemo.error.DepartmentNotFoundException;
 import com.Springboot.springbootdemo.service.DepartmentService;
-import com.Springboot.springbootdemo.service.DepartmentServiceImpl;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +14,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/departments")
-public class DepartmentController
-{
+public class DepartmentController {
     @Autowired
     private DepartmentService service;
 
@@ -24,59 +23,44 @@ public class DepartmentController
         model.addAttribute("department", new Department());
         return "index";
     }
+
     //saving department
-    @PostMapping("/save")
+    @GetMapping("/save")
     public String save(@ModelAttribute Department department) {
         service.saveDepartment(department);
-        return "redirect:/departments/form";
+        return "redirect:/departments/show";
     }
 
-
-//    @GetMapping("/list")
-//    public String departmentList(Model model) {
-//        List<Department> departments = service.fetchDepartment();
-//        model.addAttribute("departments", departments);
-//        return "department-list"; // Create an HTML template for the list
-//    }
-
-    //fetching department by getById
     @GetMapping("/{id}")
-    public String getById(@PathVariable("id") Long departmentId, Model model) throws DepartmentNotFoundException
-    {
-        try {
-        Department department = service.getDepartmentById(departmentId);
-        model.addAttribute("department", department);
-        return "department-details";
-    } catch (DepartmentNotFoundException e) {
-            return "department-not-found";
+    public String getById(@PathVariable("id") Long departmentId,Model model) throws DepartmentNotFoundException {
+        model.addAttribute("department", service.getDepartmentById(departmentId));
+        return "edit"; // Return the name of your HTML page
     }
+
+    //viewing data of department
+    @GetMapping("/show")
+    public String departmentList(Model model) {
+        List<Department> departments = service.fetchDepartment();
+        model.addAttribute("departments", departments);
+        return "show"; // Return the name of your HTML page
     }
+
 
     //Deleting department by id
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long departmentId)
-    {
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long departmentId) {
         service.deleteById(departmentId);
-        return "record deleted successfully";
+        return "redirect:/departments/show";
     }
 
     //updating record
-//    @PutMapping("/{id}")
-//    public Department update(@PathVariable Long departmentId,@RequestBody Department department)
-//    {
-//        return service.updateDepartmet(departmentId,department);
-//    }
-//    //updating record by name
-//    @GetMapping("/name/{name}")
-//    public Department updateName(@PathVariable("name") String departmentName)
-//    {
-//        return service.updateDepartmet(departmentName);
-//    }
-    @PostMapping("/update/{id}")
-    public String updateDepartment(@PathVariable("id") Long id, @ModelAttribute Department department) {
-
-        Department updatedDepartment = service.updateDepartmet(id, department);
-
-        return "redirect:/departments/list"; // Redirect to the list page after updating
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable("id") Long departmentId , @ModelAttribute Department department)
+    {
+        service.updateDepartmet(departmentId, department);
+        return "redirect:/departments/show";
     }
+
+
+
 }
